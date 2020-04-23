5c7e0fbda772a9f944051226a19145a275448287
kyuwon <kpark2477@gmail.com>
2020-04-23 16:41:37 +0900

    3rd Add gitignore and requirements.txt

--- api.py
+++ api.py
@@ -4,9 +4,17 @@ from sqlalchemy import exc
 import json
 from flask_cors import CORS
 
-from .database.models import db_drop_and_create_all, setup_db, Drink
-from .auth.auth import AuthError, requires_auth
+from database.models import db_drop_and_create_all, setup_db, Movie
+from auth.auth import AuthError, requires_auth
 
 app = Flask(__name__)
 setup_db(app)
 CORS(app)
+
+db_drop_and_create_all()
+
+@app.route('/movies')
+def get_movies():
+    selection = Movie.query.order_by(Movie.id).all()
+
+    return "hi"
