3e5d647fc72c86ef612947b28f0c488b6a33e4d3
kyuwon <kpark2477@gmail.com>
2020-04-25 00:55:22 +0900

    add authentification function

--- api.py
+++ api.py
@@ -5,7 +5,7 @@ import json
 from flask_cors import CORS
 
 from database.models import db_drop_and_create_all, setup_db, Movie, Actor
-#from auth.auth import AuthError, requires_auth
+from auth.auth import AuthError, requires_auth
 
 app = Flask(__name__)
 setup_db(app)
@@ -14,7 +14,8 @@ CORS(app)
 db_drop_and_create_all()
 
 @app.route('/movies')
-def get_movies():
+@requires_auth('get:movies')
+def get_movies(payload):
     selection = Movie.query.order_by(Movie.id).all()
     movies = [movie.format() for movie in selection]
 
@@ -25,7 +26,8 @@ def get_movies():
     })
 
 @app.route('/movies/<int:movie_id>', methods=['DELETE'])
-def delete_movie(movie_id):
+@requires_auth('delete:movies')
+def delete_movie(payload, movie_id):
     movie = Movie.query.filter(Movie.id == movie_id).one_or_none()
     
     if movie is None:
@@ -47,7 +49,8 @@ def delete_movie(movie_id):
         abort(422)
 
 @app.route('/movies', methods=['POST'])
-def create_movie():
+@requires_auth('post:movies')
+def create_movie(payload):
     body = request.get_json()
     title = body.get('title', None)
     release_date = body.get('release_date', None)
@@ -73,7 +76,8 @@ def create_movie():
         abort(422)
 
 @app.route('/movies/<int:movie_id>', methods=['PATCH'])
-def update_movie(movie_id):
+@requires_auth('patch:movies')
+def update_movie(payload, movie_id):
     movie = Movie.query.filter(Movie.id == movie_id).one_or_none()
     if movie is None:
         abort(404)
@@ -99,7 +103,8 @@ def update_movie(movie_id):
         abort(422)
 
 @app.route('/actors')
-def get_actors():
+@requires_auth('get:actors')
+def get_actors(payload):
     selection = Actor.query.order_by(Actor.id).all()
     actors = [actor.format() for actor in selection]
 
@@ -110,7 +115,8 @@ def get_actors():
     })
 
 @app.route('/actors/<int:actor_id>', methods=['DELETE'])
-def delete_actor(actor_id):
+@requires_auth('delete:actors')
+def delete_actor(payload, actor_id):
     actor = Actor.query.filter(Actor.id == actor_id).one_or_none()
     
     if actor is None:
@@ -132,7 +138,8 @@ def delete_actor(actor_id):
         abort(422)
 
 @app.route('/actors', methods=['POST'])
-def create_actor():
+@requires_auth('post:actors')
+def create_actor(payload):
     body = request.get_json()
     name = body.get('name', None)
     age = body.get('age', None)
@@ -160,7 +167,8 @@ def create_actor():
         abort(422)
 
 @app.route('/actors/<int:actor_id>', methods=['PATCH'])
-def update_actor(actor_id):
+@requires_auth('patch:actors')
+def update_actor(payload, actor_id):
     actor = Actor.query.filter(Actor.id == actor_id).one_or_none()
     if actor is None:
         abort(404)
